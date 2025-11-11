d761db489f4333e71d23cb46d9451f02b9c08c3e
poq1p <13012018g@gmail.com>
2025-11-11 10:03:33 +0200

    pong

--- bot.py
+++ bot.py
@@ -8,3 +8,15 @@ bot = commands.Bot(command_prefix='+', intents=intents)
 
 conn = sqlite3.connect('/data/bot.db')
 cursor = conn.cursor()
+
+@bot.event 
+async def on_ready():
+    cursor.execute("""CREATE TABLE IF NOT EXISTS users (
+        id BIGINT 
+        cash INT )""")
+    conn.commit()
+    print(f'{bot.user.name} ready!')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.reply(f'Понг! {round(bot.latency * 1000)} мс')

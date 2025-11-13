a834d580dafc4732a3caf5bc37db6c9abdd255ff
poq1p <13012018g@gmail.com>
2025-11-13 12:21:27 +0200

    fix

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ intents = disnake.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot()
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
